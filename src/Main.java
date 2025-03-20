55637da8a5c08e9a56ba88ddf211fbbd83bbf90a
collinhebert <collin.hebert@outlook.com>
2025-03-20 12:02:15 -0500

    The C is complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Collin");
+        System.out.println(" CCC ");
+        System.out.println("C   C ");
+        System.out.println("C     ");
+        System.out.println("C   C ");
+        System.out.println(" CCC  ");
+
     }
 }
