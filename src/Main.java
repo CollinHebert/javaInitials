b64163dc211f1ac1005b86efb7914b7383392a8f
collinhebert <collin.hebert@outlook.com>
2025-03-20 20:02:52 -0500

    Done!

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(" CCC ");
-        System.out.println("C   C ");
-        System.out.println("C     ");
-        System.out.println("C   C ");
-        System.out.println(" CCC  ");
+        System.out.println(" CCC   H   H");
+        System.out.println("C   C  H   H");
+        System.out.println("C      H   H");
+        System.out.println("C      HHHHH");
+        System.out.println("C      H   H");
+        System.out.println("C   C  H   H");
+        System.out.println(" CCC   H   H");
 
     }
 }
